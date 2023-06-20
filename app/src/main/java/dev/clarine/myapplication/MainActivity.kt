8c7f5e3f8351639dccf07a6b8b3f1a85dd042b16
clarine <antibaclarine.com>
2023-06-20 14:57:21 +0300

    first commit

--- app/src/main/java/dev/clarine/myapplication/MainActivity.kt
+++ app/src/main/java/dev/clarine/myapplication/MainActivity.kt
@@ -14,107 +14,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
-        errorMessage()
-    }
-    fun errorMessage() {
-        binding.etName.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding.etName.error = null
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
-            }
-        })
-        binding.etEmail.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding.tilEmail.error = null
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
-            }
-        })
-        binding.etphonenumber.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding.tilPhoneNumber.error = null
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
-            }
-        })
-        binding.etPassword.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding.tilPassword.error = null
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
-            }
-        })
+        setContentView(binding.root)
+//validation of the form
+ binding.btnRegister.setOnClickListener {
+     val username=binding.etName.text.toString()
+     val  phonenumber=binding.etPassword.text.toString()
+     val email=binding.etEmail.text.toString()
+     val password=binding.etPassword.text.toString()
+//     validate input fields
+     if (username.isEmpty()|| phonenumber.isEmpty()||email.isEmpty()){
+         Toast.makeText(this,"please fill in all the fields",Toast.LENGTH_SHORT).show()
+     }
+     else{
+//         navigating to MainActivity2
+         startActivity(Intent(this,MainActivity2::class.java))
+         finish()
+     }
+ }
 
     }
-    override fun onResume() {
-        super.onResume()
-//           binding .castviews()
-        binding.btnLogIn.setOnClickListener {
-            val intent= Intent(this,MainActivity2::class.java)
-            startActivity(intent)
         }
 
 
-        fun validateLogIn(){
-            val firstName=binding.etName.text.toString()
-            val lastName=binding.etEmail.text.toString()
-            val email=binding.etPassword.text.toString()
-            val password=binding.etPassword.text.toString()
-            val passwordConfirmation=binding.etPassword.text.toString()
-            var error=false
-            if (firstName.isBlank()){
-                binding.tilName.error=" Name is required"
-            }
-            if (lastName.isBlank()){
-                binding.tilEmail.error="email Name is required"
-                error=true
-            }
-            if (email.isBlank()){
-                binding.tilPhoneNumber.error="phoneNumber is required"
-                error=true
-            }
-            if (password.isBlank()){
-                binding.tilPassword.error="password is required"
-                error=true
-            }
-
-
-            if (!error){
-                Toast.makeText(this,"$firstName $lastName $email",Toast.LENGTH_LONG)
-                    .show()
-            }
-        }
-    }
 
 
 
-}
 
 
 
--- app/src/main/java/dev/clarine/myapplication/MainActivity2.kt
+++ app/src/main/java/dev/clarine/myapplication/MainActivity2.kt
@@ -1,11 +1,28 @@
 package dev.clarine.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import dev.clarine.myapplication.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding= ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            val usernameOrEmail=binding.etUserName.text.toString()
+            val password=binding.etPasswords.text.toString()
+//            validate   input fields
+            if (usernameOrEmail.isEmpty()||password.isEmpty()){
+                Toast.makeText(this,"Please fill in all fields",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            }
+        }
     }
 }
